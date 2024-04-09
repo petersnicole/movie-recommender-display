76ca6ed92d68daa5837157e8522518c82ef849a4
sarahdelaney <delaney.sar@northeastern.edu>
2024-04-08 21:42:13 -0400

    Update select component to be multiselect

--- src/RecommendedMovieDisplay.js
+++ src/RecommendedMovieDisplay.js
@@ -1,16 +1,18 @@
 import { useState, useEffect } from "react";
-import { Text, Button, VStack, Select } from "@chakra-ui/react";
+import { Text, Button, VStack } from "@chakra-ui/react";
 import Papa from 'papaparse';
+import Select from 'react-select';
 
 export function RecommendedMovieDisplay() {
   const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState('');
-  const [beforeMovie, setBeforeMovie] = useState('');
+  const [selectedMovies, setSelectedMovies] = useState([]);
+  const [beforeMovies, setBeforeMovies] = useState([]);
   const [recomendedMovie, setRecomendedMovie] = useState('');
 
-  const handleRecommend = (movie) => {
-    setBeforeMovie(movie);
-    setRecomendedMovie(movies.filter(m => m['movie name'] === movie)[0]['recomendation name']);
+  const handleRecommend = (movieChoices) => {
+    setBeforeMovies(movieChoices);
+    // TODO: change this to instead pass all movie choices
+    setRecomendedMovie(movies.filter(m => m['movie name'] === movieChoices[0])[0]['recomendation name']);
   }
 
   // copied from https://stackoverflow.com/questions/53416529/react-import-csv-file-and-parse
@@ -28,19 +30,16 @@ export function RecommendedMovieDisplay() {
     getData()
   }, []) // [] means just do this once, after initial render
 
-  
+  const options = movies.map((movie, index) => ({value: index, label: movie['movie name']}))
+
   return (
     <VStack>
-        <Button onClick={() => {handleRecommend(movies[Math.floor(Math.random() * movies.length)]['movie name'])}}>Get Recommendation for Random Movie</Button>
+        <Button onClick={() => {handleRecommend([movies[Math.floor(Math.random() * movies.length)]['movie name']])}}>Get Recommendation for Random Movie</Button>
         <Text>Or</Text>
-        <Select onChange={(event) => {setSelectedMovie(event.target.value)}} placeholder='Choose movie'>
-            {movies.map((movie, index) => { 
-                return <option key={index} value={movie['movie name']}>{movie['movie name']}</option>
-            })}
-        </Select>
-        <Button onClick={() => {handleRecommend(selectedMovie)}}>Get Recommendation</Button>
+        <div style={{ width: '100%' }}><Select onChange={(selectedOptions) => setSelectedMovies(selectedOptions.map((movie) => movie.label))} placeholder='Choose movie' options={options} isMulti/></div>
+        <Button onClick={() => {handleRecommend(selectedMovies)}}>Get Recommendation</Button>
         <Text>Recommendation for</Text>
-        <Text>{beforeMovie}</Text>
+        <Text>{beforeMovies.join(', ')}</Text>
         <Text>is</Text>
         <Text>{recomendedMovie}</Text>
     </VStack>
